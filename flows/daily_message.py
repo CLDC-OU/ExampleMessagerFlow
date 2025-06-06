75bfa07200b1b1bc7afdbe88b290fb77bc3260f9
ClaireChilders <97919301+ClaireChilders@users.noreply.github.com>
2025-06-06 14:49:20 -0400

    feat: implement daily_message.py flow

---
+++ flows/daily_message.py
@@ -0,0 +1,12 @@
+from prefect import flow, get_run_logger
+from messager.core import send_message
+
+@flow
+def daily_message_flow(env: str = "production", cfg_file: str = "configs/prod.json") -> None:
+    logger = get_run_logger()
+    logger.info(f"Starting daily flow in environment: {env} with config file: {cfg_file}")
+    message = send_message(env, cfg_file)
+    logger.info(f"Message sent: {message}")
+
+if __name__ == "__main__":
+    daily_message_flow()
